57b4a1958e278453ed9d2abf8cf72bb217b6e3f4
Victor Martins <vctr.mb@gmail.com>
2018-11-29 15:16:35 -0200

    Added Backdrop tests

---
+++ client/src/components/Backdrop/__tests__/Backdrop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup, wait } from 'test-utils';
+
+import Backdrop from '../Backdrop';
+
+afterEach(cleanup);
+
+describe('<Backdrop />', () => {
+  test('Shows and hide Backdrop', async () => {
+    const backdropId = 'backdrop';
+    const { queryByTestId, rerender } = render(<Backdrop show={true} />);
+
+    expect(queryByTestId(backdropId)).toBeInTheDocument();
+
+    rerender(<Backdrop show={false} />);
+
+    await wait(() => expect(queryByTestId(backdropId)).not.toBeInTheDocument());
+  });
+});
