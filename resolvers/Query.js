eb7421fee1d24c35620bee35ae542ff6455bfe11
vctormb <vctr.mb@gmail.com>
2018-12-01 14:13:51 -0200

    Server - Added order query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,9 +1,22 @@
+const { getUserId } = require('../utils');
+
 const products = async (parent, args, context) => {
   return await context.prisma.products({
     ...args,
   });
 };
 
+const order = async (parent, args, context) => {
+  const userId = getUserId(context);
+
+  return await context.prisma
+    .user({
+      id: userId,
+    })
+    .order();
+};
+
 module.exports = {
   products,
+  order,
 };
