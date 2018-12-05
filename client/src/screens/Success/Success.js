4f5c46e6b5aa1c044dc4984e6e968ba0ab796ee0
Victor Martins <vctr.mb@gmail.com>
2018-12-05 10:22:47 -0200

    UI - added animation in success page

--- client/src/screens/Success/Success.js
+++ client/src/screens/Success/Success.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Box, Flex } from '@rebass/grid';
+import { Transition } from 'react-transition-group';
 
 // components
 import { ScreenBox, Container, Icon, Card } from '../../components';
 // styles
 import { pxToRem, GlobalStyle } from '../../styles';
 
+const duration = 500;
+const applyTransition = state => {
+  switch (state) {
+    case 'entering':
+      return `
+        transform: translateY(10px);
+        opacity: 0 ;
+      `;
+
+    case 'entered':
+      return `
+      transform: translateY(0px);
+        opacity: 1;
+      `;
+
+    default:
+      return null;
+  }
+};
+
 const StyledCard = styled(Box)`
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: ${p => p.theme.colors.v3};
+
+  ${({ transitionState }) => applyTransition(transitionState)};
+  transition: ${`all ${duration}ms ease-in-out`};
 `;
 
 const StyledIcon = styled(Icon)`
@@ -24,7 +48,7 @@ const P = styled.p`
   color: ${p => p.theme.colors.v8};
 `;
 
-const IconWrapper = styled(Box)`
+const IconWrapper = styled(Flex)`
   box-shadow: 0 3px 14px 0 rgba(0, 0, 0, 0.1);
   border-radius: 50%;
 `;
@@ -36,13 +60,22 @@ class Success extends React.Component {
         <GlobalStyle isGreenTheme />
         <Container>
           <Flex justifyContent="center">
-            <StyledCard as={Card} p={4} flex={`0 1 ${pxToRem(400)}`}>
-              <IconWrapper mb={2}>
-                <StyledIcon icon="circlecheck-new" width="100" height="100" />
-              </IconWrapper>
-              <h2>All done!</h2>
-              <P>We received your order and it's going to arrive soon!</P>
-            </StyledCard>
+            <Transition in={true} appear={true} timeout={duration}>
+              {transitionState => (
+                <StyledCard
+                  as={Card}
+                  p={4}
+                  flex={`0 1 ${pxToRem(400)}`}
+                  transitionState={transitionState}
+                >
+                  <IconWrapper mb={2}>
+                    <StyledIcon icon="check_2" width="100" height="100" />
+                  </IconWrapper>
+                  <h2>All done!</h2>
+                  <P>We received your order and it's going to arrive soon!</P>
+                </StyledCard>
+              )}
+            </Transition>
           </Flex>
         </Container>
       </ScreenBox>
